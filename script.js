f3fc93eaf8999709d87341f49febffb6ea8a8dfc
Mohammed Khaja Mohinuddin <161879901+mohammed4721@users.noreply.github.com>
2025-12-23 22:24:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+
+setInterval(()=>{
+	const timer = document.getElementById("timer");
+	let date = new Date().toLocaleDateString();
+	let time = new Date().toLocaleTimeString();
+	timer.innerText = `${date}, ${time}`
+	
+}, 1000)
